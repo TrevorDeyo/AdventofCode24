9a61369e0547ec834291b32f72b1d75d899798a7
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-12-03 20:06:39 -0600

    Update Day1.cpp

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -3,67 +3,70 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
 int main()
 {
-    /*
-    std::ifstream file("input.txt");
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file.\n";
-        return 1;
-    }
-    
-    std::string word;
-    
-    int id = 0;
-    std::vector<int> list1;
-    std::vector<int> list2;
-    while (file >> word) {
-        if (id % 2 == 0) {
-            list1.push_back(std::stoi(word));
+    std::fstream newfile;
+    newfile.open("input.txt", std::ios::in);
+    if (newfile.is_open())
+    {
+        std::string line;
+        std::vector<int> list1;
+        std::vector<int> list2;
+        while (getline(newfile, line))
+        {
+            std::istringstream ss(line);
+            int num1, num2;
+            ss >> num1 >> num2;
+            list1.push_back(num1);
+            list2.push_back(num2);
         }
-        else {
-            list2.push_back(std::stoi(word));
+        /* MY CODE
+        std::ifstream file("input.txt");
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file.\n";
+            return 1;
         }
-        id++;
-    }
-    
-    file.close();
-    */
 
-    std::vector<int> list1 = {3, 4, 2, 1, 3, 3};
-    std::vector<int> list2 = {4, 3, 5, 3, 9, 3};
+        std::string word;
 
-    std::sort(list1.begin(), list1.end()); // Sort Ascensding Order
-    std::sort(list2.begin(), list2.end());
-
-    auto last1 = std::unique(list1.begin(), list1.end()); // Find Duplicates
-    auto last2 = std::unique(list2.begin(), list2.end());
-
-    list1.erase(last1, list1.end()); // Remove Duplicates
-    list2.erase(last2, list2.end());
-
-    // Finds how long to loop for
-    int loopCount = 0;
-    if (list1.size() > list2.size()) {
-        loopCount = list2.size();
-    }
-    else {
-        loopCount = list1.size();
-    }
-
-    // DEBUG
-    for (int i = 0; i < loopCount; i++) {
-        std::cout << list1[i] << " " << list2[i] << "\n";
-    }
-
-    // Does the math
-    int totalDistance = 0;
-    for (int i = 0; i < loopCount; i++) {
-        totalDistance += list2[i] - list1[i];
-        if (i <= 10) {
-            std::cout << totalDistance << "\n";
+        int id = 0;
+        std::vector<int> list1;
+        std::vector<int> list2;
+        while (file >> word) {
+            if (id % 2 == 0) {
+                list1.push_back(std::stoi(word));
+            }
+            else {
+                list2.push_back(std::stoi(word));
+            }
+            id++;
         }
+
+        file.close();
+        */
+        /* Example Testing
+        std::vector<int> list1 = {3, 4, 2, 1, 3, 3};
+        std::vector<int> list2 = {4, 3, 5, 3, 9, 3};
+        */
+
+        std::sort(list1.begin(), list1.end()); // Sort Ascensding Order
+        std::sort(list2.begin(), list2.end());
+
+        int n = list1.size();
+
+        // DEBUG
+        for (int i = 0; i < n; i++) {
+            std::cout << list1[i] << " " << list2[i] << "\n";
+        }
+
+        // Does the math
+        int totalDistance = 0;
+        for (int i = 0; i < n; i++) {
+            totalDistance += abs(list1[i] - list2[i]);
+        }
+        std::cout << totalDistance << "\n";
     }
-    std::cout << totalDistance << "\n";
+    newfile.close();
 }
