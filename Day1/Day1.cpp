a8a7e8ebbfe61d99994055e2308fed013128a70a
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-12-03 18:34:10 -0600

    progress but something is wrong

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream file("input.txt");
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file.\n";
+        return 1;
+    }
+
+    std::string word;
+    
+    int id = 0;
+    std::vector<int> list1;
+    std::vector<int> list2;
+    while (file >> word) {
+        if (id % 2 == 0) {
+            list1.push_back(std::stoi(word));
+        }
+        else {
+            list2.push_back(std::stoi(word));
+        }
+        id++;
+    }
+    
+    file.close();
+
+    //std::cout << list1[0] << " " << list2[0] << "\n";
+
+    std::sort(list1.begin(), list1.end());
+    std::sort(list2.begin(), list2.end());
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << list1[i] << " " << list2[i] << "\n";
+    }
+    
+    int totalDistance = 0;
+    for (int i = 0; i < list1.size(); i++) {
+        totalDistance += (list1[i] - list2[i]);
+        if (i <= 10) {
+            std::cout << totalDistance << "\n";
+        }
+    }
+    std::cout << totalDistance << "\n";
 }
